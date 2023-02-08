195ffb454ff81467d3d01070e83a4319c33d5b1c
Greenwolf25 <nicholascochrane25@gmail.com>
2023-02-08 00:03:31 -0800

    Implemented Perlin Noise

--- Terrain/compute_shaders/image_gen.glsl
+++ Terrain/compute_shaders/image_gen.glsl
@@ -21,8 +21,21 @@ uvec3 pcg3d(uvec3 v){
 }
 
 float fnoise2d(vec2 uv){
-	uvec3 input = uvec3(floatBitsToUint(uv),seed); 
-	return float(pcg3d(input).r)/MAX_UINT;
+	uvec3 input = uvec3(uint(uv.x),uint(uv.y),seed); 
+	return ((float(pcg3d(input).r)/MAX_UINT)-0.5) * 2;
+}
+
+vec2 f2noise2d(vec2 uv){
+	uvec3 input = uvec3(uint(uv.x),uint(uv.y),seed); 
+	uvec3 value = pcg3d(input);
+	return (vec2(float(value.r),float(value.g))/uvec2(MAX_UINT,MAX_UINT)-0.5) * 2;
+}
+
+vec2 hash(vec2 p ) // relies on artifacts and may give different on different systems 
+{
+	p = vec2( dot(p,vec2(127.1,311.7)), dot(p,vec2(269.5,183.3)) );
+	p = -1.0 + 2.0*fract(sin(p)*43758.5453123);
+	return sin(p*6.283 + seed);
 }
 
 float smoothNoise(vec2 uv){
@@ -54,6 +67,37 @@ float valueNoise(vec2 uv){
 	noiseValue /= 1.984375;//Normalize
 	return noiseValue;
 }
+
+
+float perlinNoise(vec2 p)
+{
+	vec2 localID = floor(p);
+    vec2 localUV = fract(p);
+    
+    vec2 weight = localUV*localUV*(3.0-2.0*localUV);
+    
+    float f00 = dot(f2noise2d(localID+vec2(0.0,0.0)),localUV-vec2(0.0,0.0));
+    float f01 = dot(f2noise2d(localID+vec2(0.0,1.0)),localUV-vec2(0.0,1.0));
+    float f10 = dot(f2noise2d(localID+vec2(1.0,0.0)),localUV-vec2(1.0,0.0));
+    float f11 = dot(f2noise2d(localID+vec2(1.0,1.0)),localUV-vec2(1.0,1.0));
+    
+    float top = mix(f00,f10,weight.x);
+    float bottom = mix(f01,f11,weight.x);
+    return mix(top,bottom,weight.y)*1.41;// 1.41 to "normalize" output to [-1,1]
+}
+
+float octPNoise(vec2 uv){
+	float noiseValue = perlinNoise(uv*4.0);
+	noiseValue += perlinNoise(uv*8.0)*0.5;
+	noiseValue += perlinNoise(uv*16.0)*0.25;
+	noiseValue += perlinNoise(uv*32.0)*0.125;
+	noiseValue += perlinNoise(uv*64.0)*0.0625;
+	noiseValue += perlinNoise(uv*128.0)*0.03125;
+	noiseValue += perlinNoise(uv*256.0)*0.015625;
+	noiseValue /= 1.984375;//Normalize
+	return noiseValue;
+}
+
 	
 void main() {
 	vec4 value = vec4(0.0, 0.0, 0.0, 1.0);
@@ -69,8 +113,8 @@ void main() {
 	float sqrBump = max(0,(pow((float(origCoord.x)/texRes.x)*2,2)) * (pow((float(origCoord.y)/texRes.y)*2,2))); // square bump
 	
 	
-	value.g = dist;
-	value.r = valueNoise(uv) - 0.5 * 2.0 * dist;
+	value.r = valueNoise(uv);
+	value.g = octPNoise(uv);
 	//value.rg = fract(uv*10);
     imageStore(imgOutput, texelCoord, value);
 }
