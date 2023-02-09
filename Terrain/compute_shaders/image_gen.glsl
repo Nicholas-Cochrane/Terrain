e906d1b808ba3422973923ffcdabd667bdc74de4
Greenwolf25 <nicholascochrane25@gmail.com>
2023-02-08 16:28:12 -0800

    implemented qunitic interpolation for Perlin noise
    qunitic is C2 continuous and thus does not have sharp acceleration changes at seems while smooth step does.

--- Terrain/compute_shaders/image_gen.glsl
+++ Terrain/compute_shaders/image_gen.glsl
@@ -7,7 +7,9 @@ uniform uvec2 texRes;
 uniform uint texHeight;
 uniform uint texWidth;
 
-const uint MAX_UINT = 4294967295;
+#define PI 3.1415926535897932384626433832795
+#define MAX_UINT 4294967295u
+#define NUM_OCTAVES 6
 
 uvec3 pcg3d(uvec3 v){
 	//Mark Jarzynski and Marc Olano, Hash Functions for GPU Rendering, Journal of Computer Graphics Techniques (JCGT), vol. 9, no. 3, 21-38, 2020
@@ -20,17 +22,6 @@ uvec3 pcg3d(uvec3 v){
 	return v;
 }
 
-float fnoise2d(vec2 uv){
-	uvec3 input = uvec3(uint(uv.x),uint(uv.y),seed); 
-	return ((float(pcg3d(input).r)/MAX_UINT)-0.5) * 2;
-}
-
-vec2 f2noise2d(vec2 uv){
-	uvec3 input = uvec3(uint(uv.x),uint(uv.y),seed); 
-	uvec3 value = pcg3d(input);
-	return (vec2(float(value.r),float(value.g))/uvec2(MAX_UINT,MAX_UINT)-0.5) * 2;
-}
-
 vec2 hash(vec2 p ) // relies on artifacts and may give different on different systems 
 {
 	p = vec2( dot(p,vec2(127.1,311.7)), dot(p,vec2(269.5,183.3)) );
@@ -38,11 +29,23 @@ vec2 hash(vec2 p ) // relies on artifacts and may give different on different sy
 	return sin(p*6.283 + seed);
 }
 
+float fnoise2d(vec2 uv){
+	uvec3 input = uvec3(floatBitsToInt(uv),seed);
+	return ((float(pcg3d(input).r)/MAX_UINT)-0.5) * 2;
+}
+
+vec2 f2noise2d(vec2 uv){
+	uvec3 input = uvec3(floatBitsToInt(uv),seed); 
+	uvec3 value = pcg3d(input);
+	return (vec2(float(value.r),float(value.g))/uvec2(MAX_UINT,MAX_UINT)-0.5) * 2;
+}
+
 float smoothNoise(vec2 uv){
 	vec2 localUV = fract(uv);
 	vec2 localID = floor(uv);
 	
 	localUV = localUV * localUV * (3.0-2.0*localUV); // quick smooth step
+	//localUV = localUV*localUV*localUV*(localUV*(localUV*6.0-15.0)+10.0); // quick qunitic
 	
 	float bl = fnoise2d(localID);
 	float br = fnoise2d(localID + vec2(1,0));
@@ -74,7 +77,8 @@ float perlinNoise(vec2 p)
 	vec2 localID = floor(p);
     vec2 localUV = fract(p);
     
-    vec2 weight = localUV*localUV*(3.0-2.0*localUV);
+    //vec2 weight = localUV*localUV*(3.0-2.0*localUV); //smoothstep
+	vec2 weight = localUV*localUV*localUV*(localUV*(localUV*6.0-15.0)+10.0); // qunitic
     
     float f00 = dot(f2noise2d(localID+vec2(0.0,0.0)),localUV-vec2(0.0,0.0));
     float f01 = dot(f2noise2d(localID+vec2(0.0,1.0)),localUV-vec2(0.0,1.0));
@@ -98,6 +102,22 @@ float octPNoise(vec2 uv){
 	return noiseValue;
 }
 
+float fbm(vec2 uv){ // fractal brownian motion
+	float noiseValue = 0.0;
+    float a = 0.5;
+
+    vec2 shift = vec2(100.0);
+    // Rotate to reduce axial bias
+    mat2 rot = mat2(cos(0.5), sin(0.5),
+                    -sin(0.5), cos(0.50));
+    for (int i = 0; i < NUM_OCTAVES; ++i) {
+        noiseValue += a * perlinNoise(uv);
+        uv = rot * uv * 2.0 + shift;
+        a *= 0.5;
+    }
+    return noiseValue;
+}
+
 	
 void main() {
 	vec4 value = vec4(0.0, 0.0, 0.0, 1.0);
@@ -112,8 +132,15 @@ void main() {
     float euclidean = max(0,(pow((float(origCoord.x)/texRes.x)*3.35,2)+ pow((float(origCoord.y)/texRes.y)*3.35,2))/sqrt(2)); //Euclidean
 	float sqrBump = max(0,(pow((float(origCoord.x)/texRes.x)*2,2)) * (pow((float(origCoord.y)/texRes.y)*2,2))); // square bump
 	
+	//for underwater shader ideas
+	//vec2 q = vec2(perlinNoise(uv*2), perlinNoise(uv*2 + vec2(1.0,1.0)));
+	//value.r = octPNoise(uv+dist*0.8);
 	
-	value.r = valueNoise(uv);
+	//for range
+	//valueNoise(uv + smoothNoise(uv*128.0)*0.002) + perlinNoise(uv*256.0)*0.00452;
+	//value.r = (max(0.005,abs(valueNoise(uv*1.2))));
+	
+	value.r = (max(0.005,abs(valueNoise(uv*1.2 + smoothNoise(uv*128.0)*0.002) + perlinNoise(uv*256.0)*0.00452)));
 	value.g = octPNoise(uv);
 	//value.rg = fract(uv*10);
     imageStore(imgOutput, texelCoord, value);
